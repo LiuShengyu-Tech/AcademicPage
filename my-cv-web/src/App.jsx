698288da1b5e1a4420c54adfedfa5eb8b1adbab6
SHENGYU LIU <45004947+LiuShengyu2018@users.noreply.github.com>
2025-02-18 20:53:54 +0100

    update

--- my-cv-web/src/App.jsx
+++ my-cv-web/src/App.jsx
@@ -4,31 +4,46 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+
+  
+    <div style={{ display: 'flex', flexDirection: 'row'}}>
+      <div style={{ width: '200px',textAlign: 'left'  }}>
+        <div>
+        
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h2>Shengyu Liu</h2>
+        <h2>刘 胜禹</h2>
+
+        <div className="card">
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <div  style={{ flex: 1 }}>
+        <h1>My CV</h1>
+        <h2>Introduction</h2>
+        
+        <p>
+           Random info: I am a software engineer with 10 years of experience in web development.   
+
+        </p>
+      </div>
+
+
+
+
+    </div>
+    
   )
 }
 
